4e4c5d778eca7c91d9896bc6e565a3770e2872bf
Pranali <87255692+Pranali1523@users.noreply.github.com>
2023-04-11 21:16:22 +0530

    Create Data_Types.js
    I have learned different data types of Java Script what we can use like int,bigint,float etc.

---
+++ Java_Script/Data_Types.js
@@ -0,0 +1,40 @@
+// Display  the output using as follows
+// console.warn("Hello")
+// console.log("World")
+
+//------------------ Data Types -----------------
+// 1> Number 
+a = 10
+console.log("data type of a is:",typeof(a))
+
+// 2> BigInt
+var x = 11111111111111111111
+console.log("data type of x is:",typeof(x))
+console.log("data type of x is now :",typeof(BigInt(x)))
+
+// 3> Boolean
+let b = true
+console.log("data type of b is:",typeof(b))
+
+// 4> Float
+f = 4.522
+var f 
+console.log("data type of f is:",typeof(f))
+
+// 5> String
+const s = "Java Script"
+console.log("data type of s is:",typeof(s))
+
+// 6> Null
+y = null
+console.log("data type of y is:",typeof(y))
+
+// 7> Date
+d = new Date(11-4-2023)
+console.log("data type of d is:",typeof(d))
+console.log(d)
+
+// 8> Array 
+arr = ["leetcode",23,33.33,true]
+console.log("data type of arr is:",typeof(arr))
+console.log(arr)
