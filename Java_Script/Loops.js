699eb53e0c4c8acd2c254db18e0e3cd52180ea4b
Pranali <87255692+Pranali1523@users.noreply.github.com>
2023-04-15 13:59:09 +0530

    Create Loops.js
    I have learned different types of Loops in Java Script like
    1> For loop
    2> While loop
    3> Do while loop

---
+++ Java_Script/Loops.js
@@ -0,0 +1,33 @@
+// ********************* Loosps *****************************
+
+// ---------------1> For loop ----------------------
+
+/* For loop is used when we know at which point we start the iteration and end 
+the iterartion foe that condition we use for loop.
+ex:- Print 1 to 5  no */
+/*for(let i=1;i<=5;i++)
+{
+    console.log(i)
+}*/
+
+//---------------  2> While loop  --------------
+
+/* It is used until to condition satisfy. 
+eg :- Print 1 to 5 nos.*/
+/*let x = 1
+while(x<=5){
+    console.log(x)
+    x++
+}*/
+
+// ---------------- 3> Do While loop -------------
+
+/*  It first print do either conditin is not satisfied first it 
+print then go to the condition.
+eg:- Print 1 to 5*/
+let x = 0
+do{
+    console.log(x)
+    x++
+}
+while(x<=5)
